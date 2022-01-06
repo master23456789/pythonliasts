03469234f7318198923c08e31eefc333bb5bb97e
master23456789 <91898768+master23456789@users.noreply.github.com>
2022-01-06 13:10:53 +0530

    Add files via upload

---
+++ pythonlists1.py
@@ -0,0 +1,8 @@
+list1 = ['science' , 'physics' , 'biology' , 'chemistry' , 1947 , 'history' , 'geography' , 2047 , 76 , 57]
+print (list1)
+print('The 4th element of the list is',list1[3])
+print('The 6th element of the list is ',list1[5])
+print('The 8th element of the list is ',list1[7])
+
+
+# 4 , 10  , 9 , 7
---
+++ pythonupdatelist.py
@@ -0,0 +1,9 @@
+# 4 , 7 , 9 , 10.
+list = [ 11 , 22 , 33 , 44 , 66 , 77 , 88 , 99 , 110 , 121]
+list[3] = 45
+list[6] = 'abcd'
+list[8] =   7
+list[9] = 11
+print (list)
+
+# HW = same and upload it on github..
